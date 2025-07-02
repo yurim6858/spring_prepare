921b88870530b19e3fe5843cabcba368c9dd8a3d
Yurim <yurim6858@g.hongik.ac.kr>
2025-07-02 11:29:47 +0900

    chore: first commit

---
+++ src/test/java/com/metaverse/spring_prepare/SpringPrepareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.metaverse.spring_prepare;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringPrepareApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
