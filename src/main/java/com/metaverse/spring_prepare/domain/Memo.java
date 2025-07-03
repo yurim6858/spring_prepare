a3e681b3a74d157191035e782ed2207a3cdf13b8
Yurim <yurim6858@g.hongik.ac.kr>
2025-07-03 15:49:33 +0900

    feat: Lombok 에너테이션 활용

---
+++ src/main/java/com/metaverse/spring_prepare/domain/Memo.java
@@ -0,0 +1,11 @@
+package com.metaverse.spring_prepare.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Memo {
+    private String username;
+    private String content;
+}
