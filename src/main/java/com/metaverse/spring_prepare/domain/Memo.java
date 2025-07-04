74e63785d7a655a2ff97240799d7328a5afb1405
Yurim <yurim6858@g.hongik.ac.kr>
2025-07-04 10:44:23 +0900

    chore: 서버 실행 포트 변경
    *8080 -> 8081
    
    Signed-off-by: Yurim <yurim6858@g.hongik.ac.kr>

--- src/main/java/com/metaverse/spring_prepare/domain/Memo.java
+++ src/main/java/com/metaverse/spring_prepare/domain/Memo.java
@@ -2,11 +2,11 @@ package com.metaverse.spring_prepare.domain;
 
 import lombok.*;
 
-@Getter
-@Setter
-@NoArgsConstructor// 기본 생성자 생성 애너테이션
-@AllArgsConstructor// 모든 필드를 파라미터로 받는 생성자
-@RequiredArgsConstructor
+//@Getter
+//@Setter
+//NoArgsConstructor// 기본 생성자 생성 애너테이션
+//@AllArgsConstructor// 모든 필드를 파라미터로 받는 생성자
+// @RequiredArgsConstructor
 public class Memo {
     private String username;
     private String content;
