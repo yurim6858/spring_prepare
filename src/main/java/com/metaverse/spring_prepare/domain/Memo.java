60ef7040f861b111f25b393aa602186d4aba2702
Yurim <yurim6858@g.hongik.ac.kr>
2025-07-04 10:37:26 +0900

    chore: Lombok 애너테이션 활용 2,3
    Signed-off-by: Yurim <yurim6858@g.hongik.ac.kr>

--- src/main/java/com/metaverse/spring_prepare/domain/Memo.java
+++ src/main/java/com/metaverse/spring_prepare/domain/Memo.java
@@ -1,10 +1,12 @@
 package com.metaverse.spring_prepare.domain;
 
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 
 @Getter
 @Setter
+@NoArgsConstructor// 기본 생성자 생성 애너테이션
+@AllArgsConstructor// 모든 필드를 파라미터로 받는 생성자
+@RequiredArgsConstructor
 public class Memo {
     private String username;
     private String content;
